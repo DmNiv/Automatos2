28355970b20221d964e66d163379f03d7e85d4a3
Arthur Elleres <arthur.elleres@itec.ufpa.br>
2023-05-21 14:48:03 -0300

    terceira questao

---
+++ terceira.py
@@ -0,0 +1,68 @@
+def Aceita(transitions, trans_saida, start, end, cadeia):
+    state = start
+    string_saida = ''
+    for simbolo in cadeia:
+        string_saida += trans_saida[state][simbolo]
+        state = transitions[state][simbolo]
+    print(string_saida)
+    return state in end
+
+
+start = 0
+end = [0, 25, 75, 100]
+trans_func = {
+    0: {
+        25: 25,
+        50: 50,
+        100: 100,
+    },
+    25: {
+        25: 50,
+        50: 75,
+        100: 25,
+    },
+    50: {
+        25: 75,
+        50: 100,
+        100: 50,
+    },
+    75: {
+        25: 100,
+        50: 25,
+        100: 75,
+    },
+    100: {
+        25: 25,
+        50: 50,
+        100: 100,
+    }
+
+}
+trans_saida = {
+    0: {
+        25: '0',
+        50: '0',
+        100: '1',
+    },
+    25: {
+        25: '0',
+        50: '0',
+        100: '1',
+    },
+    50: {
+        25: '0',
+        50: '1',
+        100: '1',
+    },
+    75: {
+        25: '1',
+        50: '1',
+        100: '1',
+    },
+    100: {
+        25: '0',
+        50: '0',
+        100: '1',
+    }
+}
+print(Aceita(trans_func, trans_saida, start, end, [50,25,50,100,25,50,100]))
