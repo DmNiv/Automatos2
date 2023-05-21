d94491cac2a4a547ee022064bab7b89049f2c505
Elton Torres <74078746+eltontorres@users.noreply.github.com>
2023-05-21 13:51:05 -0300

    Create automato_dicionario.py

---
+++ automato_dicionario.py
@@ -0,0 +1,19 @@
+#criar um dicionário em as chaves sejam as transições e os valores sejam outros dicionários contendo o caracter lido e a trasição alvo.
+
+#exemplo de dicionário.
+#imagem do automáto: https://i.stack.imgur.com/s7uO2.png
+dfa = {0:{'0':0, '1':1},
+       1:{'0':2, '1':0},
+       2:{'0':1, '1':2}}
+
+#função de validação. Ela recebe as transições, o estado inicial, um conjunto/lista contendo os estados finais e cadeia a ser lida.
+#retorna true or false
+def accepts(transitions, initial, state_final, s):
+    state = initial
+    for c in s:
+        state = transitions[state][c]
+    return state in state_final
+
+#exemplos
+print(accepts(dfa,0,{0},'1011101'))
+print(accepts(dfa,0,{0},'10111011'))
