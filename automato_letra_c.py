9548fa672472d6c04fda776ae394b3a913bebe6a
Rafael Tenorio <132300643+Tenor1o@users.noreply.github.com>
2023-05-22 21:57:03 -0300

    Update automato_letra_c.py

--- automato_letra_c.py
+++ automato_letra_c.py
@@ -1,55 +1,55 @@
-class Estado_q1:
+class Estado_q0:
+    def __init__(self):
+        self.nome = "q0"
+    
+    def transicao(self, entrada):
+        if entrada == 'a':
+            return Estado_q1()
+        elif entrada == 'b':
+            return Estado_q2()
+        else:
+            return EstadoDeRejeicao()
+
+class Estado_q1():
     def __init__(self):
         self.nome = "q1"
     
     def transicao(self, entrada):
         if entrada == 'a':
-            return Estado_q2()
-        elif entrada == 'b':
             return Estado_q3()
-        else:
-            return EstadoDeRejeicao()
-
-class Estado_q2():
-    def __init__(self):
-        self.nome = "q2"
-    
-    def transicao(self, entrada):
-        if entrada == 'a':
-            return Estado_q4()
         elif entrada == 'b':
-            return Estado_q5()
+            return Estado_q4()
         else:
             return EstadoDeRejeicao()
         
         
-class Estado_q3:
+class Estado_q2:
     def __init__(self):
-        self.nome = "q3"
+        self.nome = "q2Estado_q2"
 
     def transicao(self, entrada):
         if entrada == "a" or "b":
             return EstadoDeRejeicao()
 
-class Estado_q4:
+class Estado_q3:
     def __init__(self):
-        self.nome = "q4"
+        self.nome = "q3"
     
     def transicao(self, entrada):
         if entrada == 'a':
-            return Estado_q4()
-        elif entrada == 'b':
             return Estado_q3()
+        elif entrada == 'b':
+            return Estado_q2()
         else:
             return EstadoDeRejeicao()
         
-class Estado_q5:
+class Estado_q4:
     def __init__(self):
-        self.nome = "q5"
+        self.nome = "q4Estado_q4"
     
     def transicao(self, entrada):
         if entrada == 'b':
-            return Estado_q5()
+            return Estado_q4()
         else:
             return EstadoDeRejeicao()
 
@@ -63,7 +63,7 @@ class EstadoDeRejeicao:
 
 class Automato:
     def __init__(self):
-        self.estado_atual = Estado_q1()
+        self.estado_atual = Estado_q0()
     
     def transicoes(self, cadeia):
         for entrada in cadeia:
@@ -71,14 +71,14 @@ class Automato:
     
     def executar(self, cadeia):
         self.transicoes(cadeia)
-        if isinstance(self.estado_atual, Estado_q2):
+        if isinstance(self.estado_atual, Estado_q1):
             print("Cadeia aceita pelo automato")
-        elif isinstance(self.estado_atual, Estado_q3):
+        elif isinstance(self.estado_atual, Estado_q2):
             print("Cadeia aceita pelo automato")
-        elif isinstance(self.estado_atual, Estado_q5):
+        elif isinstance(self.estado_atual, Estado_q4):
             print("Cadeia aceita pelo automato")
         else:
             print("Cadeia rejeitada pelo automato")
 
 automato = Automato()
-automato.executar("bbbbbbb")
+automato.executar("")
